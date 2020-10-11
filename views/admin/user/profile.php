c566d20b33d49330ca3200fcc2f87befd33ad1fc
Matheus Nascimento <matheunascimentoti99@gmail.com>
2020-10-11 15:48:46 -0300

    Styled profile with Bootstrap form

--- views/admin/user/profile.php
+++ views/admin/user/profile.php
@@ -11,6 +11,7 @@ $user = $_SESSION['user'];
 <head>
     <link href="../../../Lib/bootstrap.css" rel="stylesheet">
     <link href="../../../assets/styles/page.css" rel="stylesheet">
+    <link href="../../../assets/styles/profile.css" rel="stylesheet">
     <meta charset="utf-8" />
     <title>Login</title>
 </head>
@@ -18,24 +19,43 @@ $user = $_SESSION['user'];
 <body>
 
     <?php include '../dashboard.php' ?>
-    <div class="container">
-        <form class="row mb-3" action="/Treinamento2020/user/update/<?php echo $user->getId() ?>" method="post">
-            <div class="col-md-4 col-12">
-                <input name="name" placeholder="name" value="<?php echo $user->getName() ?>">
-                <input type="email" name="email" placeholder="email" value="<?php echo $user->getEmail() ?>">
+    <h1 class="text-primary">Meu perfil</h1>
+    <div class="container page">
+        <form action="/Treinamento2020/user/update/<?php echo $user->getId() ?>" method="post">
+            <div class="form-row">
+                <div class="col-md-6 mb-3">
+                    <label for="validationServer01">Nome</label>
+                    <input name="name" placeholder="name" type="text" class="form-control" id="validationServer01" value="<?php echo $user->getName() ?>" required>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label for="validationServer02">E-mail</label>
+                    <input type="email" name="email" placeholder="email" value="<?php echo $user->getEmail() ?>" class="form-control" required>
+                </div>
             </div>
-            <select class="col-md-4"> name="type">
-                <option value="">Selecione um tipo</option>
-                <?php if ($_SESSION['user']->getType() == 'admin') { ?>
-                    <option value="admin" <?php if ($user->getType() == "admin") { ?> selected <?php } ?>>Administrador</option>
-                <?php } ?>
-                <option value="user" <?php if ($user->getType() == "user") { ?> selected <?php } ?>>Usuário comum</option>
-            </select>
-            <div class="col-md-4">
-                <input type="password" name="password">
-                <input type="password" name="password_confirmation">
+            <div class="form-row">
+                <div class="col-md-4 mb-3">
+                    <label for="validationServer03">Password</label>
+                    <input type="password" class="form-control" id="validationServer03" aria-describedby="validationServer03Feedback" name="password" required>
+
+                </div>
+                <div class="col-md-4 mb-3">
+                    <label for="validationServer03">Password</label>
+                    <input type="password" class="form-control" id="validationServer03" aria-describedby="validationServer03Feedback" name="password_confirmation" required>
+
+                </div>
+                <div class="col-md-4 mb-3">
+                    <label for="validationServer04">Selecione o tipo</label>
+                    <select class="custom-select" id="validationServer04" aria-describedby="validationServer04Feedback" required>
+                        <option selected disabled value="">Choose...</option>
+                        <?php if ($_SESSION['user']->getType() == 'admin') { ?>
+                            <option  value="admin" <?php if ($user->getType() == "admin") { ?> selected <?php } ?>>Administrador</option>
+                        <?php } ?>
+                        <option value="user" <?php if ($user->getType() == "user") { ?> selected <?php } ?>>Usuário comum</option>
+                    </select>
+                </div>
+              
             </div>
-            <button type="submit"> Alterar </button>
+            <button class="btn btn-primary" type="submit">Alterar</button>
         </form>
     </div>
 </body>
