c0153b9e88e9d35974d6fdf0f45311dc109ed131
Matheus Nascimento <matheunascimentoti99@gmail.com>
2020-10-11 15:15:36 -0300

    Improve Dashboard

--- views/admin/user/profile.php
+++ views/admin/user/profile.php
@@ -9,31 +9,35 @@ $user = $_SESSION['user'];
 <html>
 
 <head>
-    <link href="/Treinamento2020/Lib/bootstrap.css" rel="stylesheet">
-    <link href="/Treinamento2020/assets/styles/profile.css" rel="stylesheet">
+    <link href="../../../Lib/bootstrap.css" rel="stylesheet">
+    <link href="../../../assets/styles/page.css" rel="stylesheet">
     <meta charset="utf-8" />
     <title>Login</title>
 </head>
 
 <body>
 
-
     <?php include '../dashboard.php' ?>
-
-    <form action="/Treinamento2020/user/update/<?php echo $user->getId() ?>" method="post">
-        <input name="name" placeholder="name" value="<?php echo $user->getName() ?>">
-        <input type="email" name="email" placeholder="email" value="<?php echo $user->getEmail() ?>">
-        <select name="type">
-            <option value="">Selecione um tipo</option>
-            <?php if ($_SESSION['user']->getType() == 'admin') { ?>
-                <option value="admin" <?php if ($user->getType() == "admin") { ?> selected <?php } ?>>Administrador</option>
-            <?php } ?>
-            <option value="user" <?php if ($user->getType() == "user") { ?> selected <?php } ?>>Usuário comum</option>
-        </select>
-        <input type="password" name="password">
-        <input type="password" name="password_confirmation">
-        <button type="submit"> Alterar </button>
-    </form>
+    <div class="container">
+        <form class="row mb-3" action="/Treinamento2020/user/update/<?php echo $user->getId() ?>" method="post">
+            <div class="col-md-4 col-12">
+                <input name="name" placeholder="name" value="<?php echo $user->getName() ?>">
+                <input type="email" name="email" placeholder="email" value="<?php echo $user->getEmail() ?>">
+            </div>
+            <select class="col-md-4"> name="type">
+                <option value="">Selecione um tipo</option>
+                <?php if ($_SESSION['user']->getType() == 'admin') { ?>
+                    <option value="admin" <?php if ($user->getType() == "admin") { ?> selected <?php } ?>>Administrador</option>
+                <?php } ?>
+                <option value="user" <?php if ($user->getType() == "user") { ?> selected <?php } ?>>Usuário comum</option>
+            </select>
+            <div class="col-md-4">
+                <input type="password" name="password">
+                <input type="password" name="password_confirmation">
+            </div>
+            <button type="submit"> Alterar </button>
+        </form>
+    </div>
 </body>
 
 </html>
