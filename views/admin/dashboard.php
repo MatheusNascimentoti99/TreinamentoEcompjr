81735e64586c7003d8e2b4b013d5cdefa1ad375e
Matheus Nascimento <matheunascimentoti99@gmail.com>
2020-10-11 19:19:06 -0300

    Include index of users

--- views/admin/dashboard.php
+++ views/admin/dashboard.php
@@ -33,9 +33,9 @@ UserController::verifyLogin();
             }
             ?>
         </ul>
-        <text> <?php echo "Olá, " . $_SESSION['user']->getName() . "! " ?></text>
+        <text class = "nav-item"> <?php echo "Olá, " . $_SESSION['user']->getName() . "! " ?></text>
         <div id="exit">
-            <a class="text-danger" href="/Treinamento2020/user/logout">
+            <a class="text-danger nav-item" href="/Treinamento2020/user/logout">
                 <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-door-open-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M1.5 15a.5.5 0 0 0 0 1h13a.5.5 0 0 0 0-1H13V2.5A1.5 1.5 0 0 0 11.5 1H11V.5a.5.5 0 0 0-.57-.495l-7 1A.5.5 0 0 0 3 1.5V15H1.5zM11 2v13h1V2.5a.5.5 0 0 0-.5-.5H11zm-2.5 8c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z" />
                 </svg> </a>
